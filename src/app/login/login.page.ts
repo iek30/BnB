c3d530341ebc44b891a8981ae667ce2b82b00c28
iek30 <132556125+iek30@users.noreply.github.com>
2024-02-02 14:41:12 +0100

    Initial commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private router:Router) { }
+
+  iniciarSesion() {
+    this.router.navigate(['/places']);
+  }
+
+  ngOnInit() {
+  }
+
+}
